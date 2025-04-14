9fd6ac5773d0f1b334b17e22fb7f40efd4d3b50d
Mohamad bin Idrus <mohamad_idrus@student.uns.ac.id>
2025-04-14 16:55:22 +0700

    add controller Laundry

---
+++ app/Http/Controllers/Api/LaundryController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+//import model Laundry
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LaundryController extends Controller
+{
+    //
+}
